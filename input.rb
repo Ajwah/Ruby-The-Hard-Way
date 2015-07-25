989bae4334aa1da77c6b688e151c7f5eedb85c0d
Ajwah <johnson786.kevin+git@gmail.com>
2015-07-25 10:59:02 -0400

    Implement ex. 11, gets.chomp

---
+++ input.rb
@@ -0,0 +1,8 @@
+puts "What is your name?"
+name = gets.chomp
+puts "What is your age?"
+age = gets.chomp
+puts "What is your gender?"
+gender = gets.chomp
+
+puts "Hello, #{name}, you are a #{gender} aged #{age}."
